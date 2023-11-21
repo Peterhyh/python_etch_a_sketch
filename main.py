a50d7ffe3b3c92baaa50e714341bcc5495d04c5e
Peter Huynh <peterhyh@yahoo.com>
2023-11-20 19:57:41 -0600

    all four moving directions added

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ def move_forwards():
     player.fd(10)
 
 
+def move_backwards():
+    player.bk()
+
+
+def move_left():
+    player.lt()
+
+
+def move_right():
+    player.rt()
+
+
 screen = Screen()
 screen.onkey(move_forwards, "space")
 screen.exitonclick()
